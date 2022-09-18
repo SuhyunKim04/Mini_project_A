40cebf247a62bc390a2b6c45a1e5bb2f2fbd98e6
Suhyun Kim <kim00431@algonquinlive.com>
2022-09-17 21:33:26 -0400

    20220917

---
+++ js/common.js
@@ -0,0 +1,43 @@
+const header = document.querySelector('.page_header')
+window.addEventListener('scroll', () => {
+    if(window.pageYOffset > 270){
+        header.classList.add('sticky')
+    }else{
+        header.classList.remove('sticky')
+    }
+    
+})
+
+
+
+const btn = document.querySelector(".btnScroll")
+
+btn.addEventListener('click', ()=> {
+    console.log(window.pageYOffset)
+})
+
+const add = document.querySelector('.add')
+add.addEventListener('click', ()=>{
+    header.classList.add("sticky")
+})
+
+const remove = document.querySelector('.remove')
+remove.addEventListener('click', ()=>{
+    header.classList.remove("sticky")
+})
+
+
+// const btnClass = document.querySelectorAll('.btnClass');
+// btnClass.addEventListener('click',(e) => {
+
+// })
+
+const change = document.querySelector('.btnImage')
+const big = document.querySelector('.bigImg');
+const test = document.querySelector('.test')
+big.setAttribute('src','a.jpg')
+
+change.addEventListener('click', ()=> {
+    big.setAttribute('src', './images/profile_${i}_big.jpg')
+})
+
