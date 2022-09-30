045fb0df7490cd19586caf6e7b32475cf761d07d
Suhyun Kim <kim00431@algonquinlive.com>
2022-09-29 23:45:37 -0400

    20220929

--- js/common.js
+++ js/common.js
@@ -1,74 +1,50 @@
-const header = document.querySelector('.page_header')
-window.addEventListener('scroll', () => {
-    if(window.pageYOffset > 270){
-        header.classList.add('sticky')
-    }else{
-        header.classList.remove('sticky')
-    }
-    
-})
+function scrollHeader () {
+    const header = document.querySelector('.page_header')
+    window.addEventListener('scroll', () => {
+        console.log(window.pageYOffset);
+        if(window.pageYOffset > 270){
+            header.classList.add('sticky')
+        }else{
+            header.classList.remove('sticky')
+        }
+        
+    })
+}
 
-
-
-// const btn = document.querySelector(".btnScroll")
-
-// btn.addEventListener('click', ()=> {
-//     console.log(window.pageYOffset)
-// })
-
-// const add = document.querySelector('.add')
-// add.addEventListener('click', ()=>{
-//     header.classList.add("sticky")
-// })
-
-// const remove = document.querySelector('.remove')
-// remove.addEventListener('click', ()=>{
-//     header.classList.remove("sticky")
-// })
-
-
-// const btnClass = document.querySelectorAll('.btnClass');
-// btnClass.addEventListener('click',(e) => {
-
-// })
-
-const change = document.querySelector('.btnImage')
-const big = document.querySelector('.bigImg');
-const test = document.querySelector('.test')
+scrollHeader();
  
+const getOffset = () => {
+    const section = document.querySelector('.gallery') 
+    return section.offsetTop;
+} 
 
-// change.addEventListener('click', ()=> {
-//     big.setAttribute('src', './images/profile_${i}_big.jpg')
-// })
-
-test.addEventListener('click', (e)=> {
-    console.log(e.target)
-    const src = test.getAttribute('src')
+const viewProfile = () => {
+    const change = document.querySelector('.btnImage') 
+    const big = document.querySelector('.bigImg');
+}
+  
+ 
+const viewGallery = () => {
+    const position = getOffset()
+    const dimm = document.querySelector('.dimm')
+    const view = document.querySelector('.view_detail')
+    const gallery =document.querySelector('.gallery .list a');
+    gallery.addEventListener('click',(e) => {
+        // e.preventDefault();
+        dimm.style.display = 'block'
+        window.scrollTo(0, position);
+        view.classList.add('open')
+        console.log(view)
+    })
     
-}) 
-
-const pets = ['puppy','cat','lion','tiger','wale','rabbit']
-for(let i = 0; i < pets.length; i++){
-    console.log(pets[i])
+    const close = document.querySelector('.close')
+    close.addEventListener('click',(e) => {
+        view.classList.remove('open')
+        dimm.style.display = 'none'
+    })
 }
 
-for(const pet of pets) {
-    console.log(pet)
-}
-for(const i in pets) {
-    console.log(i)
-}
-
-pets.forEach( (e,idx) => {
-    console.log(e, idx)
+viewGallery();
+window.addEventListener('resize', function() {
+    viewGallery(); 
 })
-
-const view = document.querySelector('.contents_detail')
-const gallery =document.querySelector('.gallery .list a');
-gallery.addEventListener('click',(e) => {
-    // e.preventDefault();
-    gallery.classList.add('view')
-    console.log(view)
-})
-
-
