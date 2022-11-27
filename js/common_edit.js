f9e8b420be20bec3020e39d7fde1f27bf15d7bba
Suhyun Kim <kim00431@algonquinlive.com>
2022-11-26 21:22:26 -0500

    20221126

--- js/common_edit.js
+++ js/common_edit.js
@@ -1,3 +1,11 @@
+function setVh () {
+    let vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty("--vh", `${vh}px`);
+}
+
+setVh();
+
+
 function scrollHeader () {
     const header = document.querySelector('.page_header')
     window.addEventListener('scroll', () => {
